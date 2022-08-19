3afd363fb40ce534e7e22650a89d0a0777659c0d
HectorMau501 <mauhector7@gmail.com>
2022-08-19 17:42:23 -0500

    66 sobrecarga de metodos

--- src/ClasesYObjectos/Main.java
+++ src/ClasesYObjectos/Main.java
@@ -9,17 +9,10 @@ public class Main {
     
     public static void main(String [] agrs)
     {
-        int n1 =  Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
-        int n2 =  Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo numero"));
+        Persona p1 = new Persona("Hector",18);
         
+        p1.mostrarDatos();
         
-        Operacion operacion1 = new Operacion();
-        
-        System.out.println("El resultado de la divison es: "+operacion1.division(n1, n2));
-        System.out.println("El resultado de la multiplicacion es: "+operacion1.multiplicacion(n1, n2)); 
-        System.out.println("E; resultado de la resta es: "+operacion1.restar(n1,n2));
-        System.out.println("El resultado de la suma es: "+operacion1.sumar(n1,n2));
-       
     }
 }
 
---
+++ src/ClasesYObjectos/Persona.java
@@ -0,0 +1,24 @@
+package ClasesYObjectos;
+
+
+public class Persona 
+{
+    
+    private String nombre;
+    private int edad;
+    
+    
+    public Persona(String nombre, int edad)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+    }        
+    public void mostrarDatos()
+    {
+        System.out.println("El nombre es: "+nombre);
+        System.out.println("La edad es: "+edad);
+    }
+    
+    
+    
+}
